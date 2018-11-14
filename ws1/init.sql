71b83724e4f88ebe2b31769b26fc4f5712e9d105
Jules LE BRIS <jls.lebris@gmail.com>
2018-11-14 13:22:01 +0100

    init db

---
+++ ws1/init.sql
@@ -0,0 +1,18 @@
+DROP TABLE public."Livre";
+
+CREATE TABLE public."Livre"
+(
+    id integer NOT NULL,
+    "titre" character varying COLLATE pg_catalog."default" NOT NULL,
+    "auteur" character varying COLLATE pg_catalog."default" NOT NULL,
+    "resume" character varying COLLATE pg_catalog."default",
+	"proprietaire_id" integer,
+    CONSTRAINT "Livre_pkey" PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public."Livre"
+    OWNER to ripoul;
